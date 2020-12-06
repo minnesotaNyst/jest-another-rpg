4f46c236d4890faf626a71da1171b28ea9c8b931
minnesotaNyst <71350960+minnesotaNyst@users.noreply.github.com>
2020-12-06 17:34:08 -0600

    missing a potion test

--- __tests__/Potion.test.js
+++ __tests__/Potion.test.js
@@ -1,5 +1,12 @@
 const Potion = require('../lib/Potion.js');
 
+test('gets a description of the enemy', () => {
+	const enemy = new Enemy('goblin', 'sword');
+
+	expect(enemy.getDescription()).toEqual(expect.stringContaining('goblin'));
+	expect(enemy.getDescription()).toEqual(expect.stringContaining('sword'));
+});
+
 test('creates a random potion object', () => {
 	const potion = new Potion();
 
